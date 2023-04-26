8c27d91b251b4e74858145d8e132a17a47343f71
Sourcery AI <>
2023-04-26 23:53:28 +0000

    'Refactored by Sourcery'

--- scripts/local.datalake.RemoveIamAllowedPrincipals.py
+++ scripts/local.datalake.RemoveIamAllowedPrincipals.py
@@ -7,18 +7,16 @@ lf = boto3.client('lakeformation')
 
 permissions = []
 
-permissionResp = lf.list_permissions() 
+permissionResp = lf.list_permissions()
 permissions.extend(permissionResp['PrincipalResourcePermissions'])
 
 while 'NextToken' in permissionResp:
     print(permissionResp)
     permissionResp = lf.list_permissions(NextToken=permissionResp['NextToken']) 
     permissions.extend(permissionResp['PrincipalResourcePermissions'])
-    
-progress = 0    
-for grant in permissions:
+
+for progress, grant in enumerate(permissions):
     print("\r"+str(progress)+"/"+str(len(permissions)), end='')
-    progress += 1
     if(grant['Principal']['DataLakePrincipalIdentifier'] == "IAM_ALLOWED_PRINCIPALS"):
         lf.revoke_permissions(
             Principal=grant["Principal"],
